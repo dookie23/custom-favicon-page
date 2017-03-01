156135f3238e7cab7ec619a9ea74cec36f8f371f
Agustín Cañas <agustincanas@gmail.com>
2017-03-01 08:36:15 +0100

    Showing target page

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,4 +1,5 @@
-var faviconUrl = location.search.split('favicon-url=')[1];
+var faviconUrl = location.search.split('favicon-url=')[1] ? location.search.split('favicon-url=')[1].split('&')[0] : null;
+var targetUrl = location.search.split('target-url=')[1] ? location.search.split('target-url=')[1].split('&')[0] : null;
 
 if (faviconUrl) {
   var link = document.createElement('link');
@@ -6,10 +7,11 @@ if (faviconUrl) {
   link.rel = 'shortcut icon';
   link.href = faviconUrl;
   document.head.appendChild(link);
-  
-  // var faviconImg = document.createElement('img');
-  // faviconImg.setAttribute('src', faviconUrl);
-  // faviconImg.setAttribute('id', 'favicon-img');
-  
-  // document.getElementsByClassName('page-header')[0].appendChild(faviconImg);
+}
+
+if (targetUrl) {
+  var iframe = document.createElement('iframe');
+  iframe.src = targetUrl;
+  iframe.frameborder='0';
+  document.body.appendChild(iframe);
 }
