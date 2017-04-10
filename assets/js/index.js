8768a79d8d727e5832e436199264c63760f5f8fd
Agustín Cañas <agustincanas@gmail.com>
2017-04-10 11:00:23 +0200

    Update index.js

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,6 +1,13 @@
 var faviconUrl = location.search.split('favicon-url=')[1] ? location.search.split('favicon-url=')[1].split('&')[0] : null;
 var targetUrl = location.search.split('target-url=')[1] ? location.search.split('target-url=')[1].split('&')[0] : null;
 var iframeOption = location.search.split('iframe-option=')[1] ? location.search.split('iframe-option=')[1].split('&')[0] : null;
+var redirectionTimeout = location.search.split('redirection-timeout=')[1] ? parseInt(location.search.split('redirection-timeout=')[1].split('&')[0]) : 0;
+
+// Redirection function
+function redirect(url) {
+    window.location = url;
+};
+
 
 if (faviconUrl) {
   var link = document.createElement('link');
@@ -15,5 +22,5 @@ if (targetUrl) {
   iframe.src = targetUrl;
   iframe.frameborder='0';
   if (iframeOption) document.body.appendChild(iframe);
-  else window.location = targetUrl;
+  else setTimeout(redirect(targetUrl),redirectionTimeout)
 }
